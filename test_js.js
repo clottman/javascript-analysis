23221331522c5eea6917d0ecb912a3108fda18f4
clottman <clottman@live.com>
2014-10-14 13:35:35 -0500

    generalize whitelist method for use by whitelist and blacklist; create new whitelist and blacklist methods as a wrapper for the generic one

--- test_js.js
+++ test_js.js
@@ -1,60 +1,81 @@
 // depends on esprima.js
 
 // code: a string of javascript code to be tested
-// whitelist_params: an array of strings representing something the javascript must contain
+// ast_types: an array of strings representing something the javascript must contain or not contain
+// should_contain: true to check if the ast_types are in the code_str, false to check whether they are not
 // returns false if unable to parse code_str, a hash of whitelist params and whether they were in the code if not
-var whitelist = function(code_str, whitelist_params) {
+var check_presence = function(code_str, ast_types, should_contain) {
 	try {
 		var code_tree = esprima.parse(code_str).body;
 	} catch (e) {
 		return false;
 	}
-	var whitelist_length = whitelist_params.length;
+	var types_length = ast_types.length;
 	var num_found = 0;
 	
 	// hash of parameters initialized to false
-	var whitelist_hash = createHash(whitelist_params, false);
+	var types_hash = createHash(ast_types, false);
 	
 	// copy the code_tree into a queue
 	var nodes = code_tree.slice();
 
 	// start checking nodes, stop when out of nodes or all parameters have been found
 	var i = 0;
-	while (num_found != whitelist_length && i < nodes.length ) {
-		if (whitelist_hash[nodes[i].type] == false) {
-			whitelist_hash[nodes[i].type] = true;
-			num_found++;
-		}
-
-		// push child nodes onto queue to check at the end
-		// uses breadth first search to check nodes
-		if (nodes[i].consequent != undefined) {
-			var new_nodes = nodes[i].consequent.body;
-			new_nodes.push.apply(nodes, new_nodes);
-		}
-		if (nodes[i].body != undefined) {
-			if (isArray(nodes[i].body)) {
-				var new_nodes = nodes[i].body;
-				new_nodes.push.apply(nodes, new_nodes);
-			} else {
-				nodes.push(nodes[i].body)
+	while (i < nodes.length ) {
+		if (should_contain && num_found != types_length) {
+			if (types_hash[nodes[i].type] == false) {
+				types_hash[nodes[i].type] = true;
+				num_found++;
 			}
+			// push child nodes onto queue to check at the end
+			// uses breadth first search to check nodes
+			pushChildren(nodes, nodes[i]);
+			i++;
+		} else {
+			// if whitelisting types, break when you've found them all
+			break;
 		}
-		if (nodes[i].declarations != undefined) {
-			var new_nodes = nodes[i].declarations;
-			new_nodes.push.apply(nodes, new_nodes);
-		}
-		if (nodes[i].type == "VariableDeclarator") {
-			nodes.push(nodes[i].init);
-		}
-		i++;
 	}
 
-	return whitelist_hash;
+	return types_hash;
 }
 
 var blacklist = function(code_str, blacklist_params) {
-	
+	var result = check_presence(code_str, blacklist_params, false);
+	return result;	
+}
+
+// code_str: a string of javascript code to be tested
+// whitelist_params: an array of strings representing something the javascript must contain
+// returns false if unable to parse code_str, a hash of whitelist params and whether they were in the code if not
+var whitelist = function(code_str, whitelist_params) {
+	var result = check_presence(code_str, whitelist_params, true);
+	return result;
+}
+
+// NOTE: Traversing the tree ended up being more complicated than I thought. Not all children are in node.body!
+// For a more robust solution, I'd use something like https://www.npmjs.org/package/esprima-walk
+// or https://github.com/Constellation/estraverse
+var pushChildren = function(nodes, node) {
+	if (node.consequent != undefined) {
+		var new_nodes = node.consequent.body;
+		new_nodes.push.apply(nodes, new_nodes);
+	}
+	if (node.body != undefined) {
+		if (isArray(node.body)) {
+			var new_nodes = node.body;
+			new_nodes.push.apply(nodes, new_nodes);
+		} else {
+			nodes.push(node.body)
+		}
+	}
+	if (node.declarations != undefined) {
+		var new_nodes = node.declarations;
+		new_nodes.push.apply(nodes, new_nodes);
+	}
+	if (node.type == "VariableDeclarator") {
+		nodes.push(node.init);
+	}
 }
 
 var isArray = function(o) {
